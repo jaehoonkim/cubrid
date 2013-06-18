7b34d7d91592814fa6de2dcaa31ff5a2d6e297e9
redef <jaehoon@gmail.com>
2013-06-19 01:56:52 +0900

    Next()의 기본 구조만,,

--- rows.go
+++ rows.go
@@ -5,11 +5,17 @@ package cubrid
 char* ex_cci_get_result_info_name(T_CCI_COL_INFO* res_info, int index) {
 	return CCI_GET_RESULT_INFO_NAME(res_info, index);
 }
+
+T_CCI_U_TYPE ex_cci_get_result_info_type(T_CCI_COL_INFO* res_info, int index) {
+	return CCI_GET_RESULT_INFO_TYPE(res_info, index);
+}
 */
 import "C"
 import (
 	"database/sql/driver"
 	"fmt"
+	"io"
+	"unsafe"
 )
 
 type cubridRows struct {
@@ -42,6 +48,39 @@ func (rows *cubridRows) Close() error {
 }
 
 func (rows *cubridRows) Next(dest []driver.Value) error {
+	var err C.int
+	var cci_error C.T_CCI_ERROR
+	var col_info *C.T_CCI_COL_INFO
+	var stmt_type C.T_CCI_CUBRID_STMT
+	var col_count C.int
+
+	err = C.cci_cursor(rows.s.req, 1, C.CCI_CURSOR_CURRENT, &cci_error)
+	if err == C.CCI_ER_NO_MORE_DATA {
+		return io.EOF
+	}
+	if int(err) < 0 {
+		return fmt.Errorf("cursor err: %d, %s", cci_error.err_code, cci_error.err_msg)
+	}
+	
+	err = C.cci_fetch(rows.s.req, &cci_error)
+	if int(err) < 0 {
+		return fmt.Errorf("fetch err: %d, %s", cci_error.err_code, cci_error.err_msg)
+	}
+	
+	col_info = C.cci_get_result_info(rows.s.req, &stmt_type, &col_count)
+	var columnType C.T_CCI_U_TYPE
+	var i C.int
+	var value C.void
+	var ind C.int
+	for i = C.int(0); i < col_count; i++ {
+		columnType = C.ex_cci_get_result_info_type(col_info, i)
+		switch columnType {
+		case C.CCI_U_TYPE_CHAR:
+			C.cci_get_data(rows.s.req, i, C.CCI_A_TYPE_STR, unsafe.Pointer(&value), &ind)
+		case C.CCI_U_TYPE_INT:
+			C.cci_get_data(rows.s.req, i, C.CCI_A_TYPE_INT, unsafe.Pointer(&value), &ind)
+ 
+		}
+	}
 	return nil
 }
-
